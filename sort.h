3b00646748a5233b99b30cdfe9572a363d3b8f70
afinesami <samibenomar84@gmail.com>
2020-03-23 22:09:53 +0000

    sort

--- sort.h
+++ sort.h
@@ -6,11 +6,11 @@
 #include <string.h>
 
 /**
- * struct listint_s - Doubly linked list
+ * struct listint_s - Doubly linked list node
  *
- * @n: Integer stored
- * @prev: Pointer element of the list
- * @next: Pointer the next element of list
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -19,9 +19,63 @@ typedef struct listint_s
   struct listint_s *next;
 } listint_t;
 
+/**
+ * enum kind_e - enum for the cards
+ * @SPADE: group for spade cards
+ * @HEART: group for heart cards
+ * @CLUB: group for club cards
+ * @DIAMOND: group for diamond cards
+ */
+
+typedef enum kind_e
+  {
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+  } kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+  const char *value;
+  const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+  const card_t *card;
+  struct deck_node_s *prev;
+  struct deck_node_s *next;
+} deck_node_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 #endif
